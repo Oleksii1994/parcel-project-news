be20119c03d1ae93efb67f257e52b767d115737f
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-03-21 01:43:50 +0200

    init & start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './js/mobile-menu';
+import './js/devices-responsive';
+import './js/input-actions';
+import './js/switch-theme';
+import './js/calendar';
+import './js/weather';
+import './js/categories-btns';
+import './js/add-to-read';
+import './js/add-to-favorite';
+import './js/pagination';

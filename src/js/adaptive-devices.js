d64987e3428fb3c2b29b0be2a58d60030d0209cc
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-03-21 19:06:37 +0200

    fix adaptive devices

--- src/js/adaptive-devices.js
+++ src/js/adaptive-devices.js
@@ -1,29 +1,29 @@
 const getDevice = [
-    { width: 640, useClass: 'mobile' },
-    { width: 1024, useClass: 'tablet' },
-  ];
-  
-  window.addEventListener('resize', onWidthUpdate);
-  
-  function onWidthUpdate() {
-    const screens = getdevice;
-    const widthDevices = window.innerWidth; 
-    const foundScreen = screens.find(option => widthDevices <= option.width);
-    const defaultScreenClass = 'default';
-    let result;
-  
-    if (foundScreen) {
-      result = foundScreen.useClass;
-    } else {
-      result = defaultScreenClass;
-    }
-  
- document.body.classList.forEach(className => {
-    if (className.startsWith('screen-')) {
-      document.body.classList.remove(className);
-    }
-  });
+  { width: 640, useClass: 'mobile' },
+  { width: 1024, useClass: 'tablet' },
+];
 
-  document.body.classList.add(`screen-${result}`);
+window.addEventListener('resize', onWidthUpdate);
 
+function onWidthUpdate() {
+  const screens = getDevice;
+  const widthDevices = window.innerWidth; 
+  const foundScreen = screens.find(option => widthDevices <= option.width);
+  const defaultScreenClass = 'default';
+  let result;
+
+  if (foundScreen) {
+    result = foundScreen.useClass;
+  } else {
+    result = defaultScreenClass;
   }
+
+document.body.classList.forEach(className => {
+  if (className.startsWith('screen-')) {
+    document.body.classList.remove(className);
+  }
+});
+
+document.body.classList.add(`screen-${result}`);
+
+}

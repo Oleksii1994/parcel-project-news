44ac8a3dc003c78002ba2ee6b2d6e5d9eb73b41d
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-03-22 14:57:52 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/adaptive-devices';
 import './js/mobile-menu';
 import './js/devices-responsive';
 import './js/input-actions';
-import './js/switch-theme';
+// import './js/switch-theme';
 import './js/calendar';
 import './js/weather';
 import './js/categories-btns';

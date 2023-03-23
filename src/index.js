3f442120dd1775645848a74fa5af0ee51b05f906
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-03-23 23:32:47 +0200

    read

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './js/adaptive-devices';
-import './js/API/fetchAPI';
-import './js/mobile-menu';
+// import './js/API/fetchAPI';
+// import './js/mobile-menu';
 
-import './js/input-actions';
-import './js/switch-theme';
-import './js/newCalendar';
+// import './js/input-actions';
+// import './js/switch-theme';
+// import './js/newCalendar';
+import './js/read';
 // import './js/weather';
-import './js/categories-btns';
-import './js/add-to-read';
-// import './js/add-to-favorite';
-import './js/pagination';
-import './js/header';
+// import './js/categories-btns';
+// import './js/add-to-read';
+// // import './js/add-to-favorite';
+// import './js/pagination';
+// import './js/header';

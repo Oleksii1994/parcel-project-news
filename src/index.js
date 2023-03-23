b6afba52a997ce742d618dee0854f8b0e23d47c8
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-03-23 23:44:46 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/adaptive-devices';
-import './js/API/fetchAPI';
-import './js/mobile-menu';
+// import './js/API/fetchAPI';
+// import './js/mobile-menu';
 
 import './js/API/api-data-normalaizer';
 import './js/input-actions';
@@ -9,6 +9,7 @@ import './js/newCalendar';
 // import './js/weather';
 import './js/categories-btns';
 import './js/add-to-read';
+import './js/read';
 // import './js/add-to-favorite';
 import './js/pagination';
 import './js/header';

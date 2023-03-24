8225672556a33a2735520fdeef3593a8de0fec37
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-03-24 13:57:34 +0100

    bug fix

--- src/js/API/fetchAPI.js
+++ src/js/API/fetchAPI.js
@@ -5,7 +5,6 @@ class NewsAPIService {
   #BASE_URL = 'https://api.nytimes.com/svc/';
   #SEARCH_NEWS_PATH = 'search/v2/articlesearch.json?';
   #API_KEY = 'kkEdLmiWAben4vvAV9iKuhykdEAlksXW';
-  #MOST_POPULAR_NEWS_PATH = `mostpopular/v2/viewed/7.json?`;
 
   constructor() {
     this.searchQuery = '';
@@ -56,7 +55,6 @@ class NewsAPIService {
     const response = await fetch(URL);
     this.errorHandle(response, response.statusText);
     const { results } = await response.json();
-    console.log(selectedDate);
     return results;
   }
 

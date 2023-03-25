4653ae4e211cd963fedcfb877a3b3886ae0c8f4d
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-25 15:34:03 +0200

    favorite add

--- src/js/render news gallery/render-news-gallery.js
+++ src/js/render news gallery/render-news-gallery.js
@@ -1,8 +1,10 @@
 import { newsApi } from '../API/fetchAPI';
 import { NormalizeData } from '../API/api-data-normalaizer';
+import { onLoadHomePage } from '../add-to-favorite'; //////////////
 import { refs } from '../refs/refs';
 import { markup } from '../renderMarkup';
 import { Notify } from 'notiflix';
+
 import { input } from '../newCalendar';
 import { selectedDate } from '../newCalendar';
 import _debounce from 'debounce';
@@ -18,6 +20,7 @@ async function pageLoadHandler() {
       'beforeend',
       markup.createGalleryCardMarkup(NormalizeData.popularData(results))
     );
+    onLoadHomePage(); /////////////////////////////////
   } catch (error) {
     console.log(error);
     Notify.failure(`${error}`);
@@ -39,3 +42,47 @@ function onCalendarChange(e) {
     markup.createGalleryCardMarkup(filteredNews)
   );
 }
+
+
+
+// import { newsApi } from '../API/fetchAPI';
+// import { NormalizeData } from '../API/api-data-normalaizer';
+// import { refs } from '../refs/refs';
+// import { markup } from '../renderMarkup';
+// import { Notify } from 'notiflix';
+// import { input } from '../newCalendar';
+// import { selectedDate } from '../newCalendar';
+// import _debounce from 'debounce';
+
+// window.addEventListener('load', pageLoadHandler, { once: true });
+// input.addEventListener('change', _debounce(onCalendarChange, 1500));
+
+// async function pageLoadHandler() {
+//   try {
+//     const { results, num_results } = await newsApi.fetchPopularArticles();
+//     newsApi.newsDataArr = NormalizeData.popularData(results);
+//     refs.galleryEl.insertAdjacentHTML(
+//       'beforeend',
+//       markup.createGalleryCardMarkup(NormalizeData.popularData(results))
+//     );
+//   } catch (error) {
+//     console.log(error);
+//     Notify.failure(`${error}`);
+//   }
+// }
+
+// function onCalendarChange(e) {
+//   const filteredNews = newsApi.newsDataArr.filter(item => {
+//     const dateString = NormalizeData.formatDateToString(item.date);
+//     if (selectedDate[1] === '') {
+//       return dateString === selectedDate[0];
+//     } else {
+//       return dateString >= selectedDate[0] && dateString <= selectedDate[1];
+//     }
+//   });
+//   markup.clearMarkup(refs.galleryEl);
+//   markup.renderMarkup(
+//     refs.galleryEl,
+//     markup.createGalleryCardMarkup(filteredNews)
+//   );
+// }

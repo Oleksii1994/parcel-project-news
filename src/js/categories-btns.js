8854ed96e9a36483c6a86238b4ee34e3e4afbf0a
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-03-26 14:04:28 +0200

    refactoring categories

--- src/js/categories-btns.js
+++ src/js/categories-btns.js
@@ -377,19 +377,7 @@ async function onOtherBoxClick(e) {
   categoriesOtherTextRef.textContent = e.target.textContent;
   categoriesOtherBtnRef.classList.remove('visible');
 
-  newsApi.currentCategory = e.target.textContent.toLowerCase();
-  try {
-    const data = await newsApi.fetchArticlesByCategory();
-    newsApi.newsDataArr = NormalizeData.categoryData(data);
-    markup.clearMarkup(refs.galleryEl);
-    markup.renderMarkup(
-      refs.galleryEl,
-      markup.createGalleryCardMarkup(NormalizeData.categoryData(data))
-    );
-  } catch (error) {
-    console.log(error);
-    Notify.failure(`${error}`);
-  }
+  categoriesFetchNewsService(e);
 }
 
 async function onBtnsClick(e) {
@@ -398,6 +386,10 @@ async function onBtnsClick(e) {
   }
   e.target.classList.add('active');
 
+  categoriesFetchNewsService(e);
+}
+
+async function categoriesFetchNewsService(e) {
   newsApi.currentCategory = e.target.textContent.toLowerCase();
   try {
     const data = await newsApi.fetchArticlesByCategory();

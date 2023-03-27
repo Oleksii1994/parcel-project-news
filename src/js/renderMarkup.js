cc1e575e899196bc5262fdb2fcdaf6d593b895e4
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-03-27 16:37:09 +0300

    Update renderMarkup.js

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -64,3 +64,38 @@ export const markup = {
   },
 };
 
+export const markupForFavoritesAndRead = {
+  createGalleryCardMarkup(arr) {
+    return arr
+      .map(({ img, title, text, date, id, category, url }, index) => {
+        return `<li id="${id}" class="gallery__item">
+  <div class="gallery-thumb" style="background-image: url('${img}')">
+      <p class="gallery-thumb__subtitle">${category}</p>
+      <p class="gallery-thumb__already">
+        Already read
+      </p>
+    ${
+      checkPresentArticle(id)
+        ? '<button type="button" class="gallery-thumb__btn"><p class="gallery-thumb__name add">Remove from favorite<span class="gallery-thumb__icon">&#10084;</span></p></button>'
+        : '<button type="button" class="gallery-thumb__btn"><p class="gallery-thumb__name add">Add to favorite<span class="gallery-thumb__icon">&#9825;</span></p></button>'
+    }
+  </div>
+    <h3 class="gallery__title">${title}</h3>
+    <p class="gallery__text">${text}</p>
+      <div class="thumb">
+        <p class="thumb__text">${date}</p>
+        <a class="thumb__link" href="${url}" target="_blank">Read more</a>
+      </div>
+</li>`;
+      })
+      .join('');
+  },
+
+  clearMarkup(ref) {
+    ref.innerHTML = '';
+  },
+
+  renderMarkup(ref, markup) {
+    ref.insertAdjacentHTML('beforeend', markup);
+  },
+};

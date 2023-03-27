d48b7c2fba4999733f563af8960be62e0e39209c
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-27 20:18:12 +0300

    uppdate header

---
+++ src/read.js
@@ -0,0 +1,9 @@
+import './js/add-to-read';
+import './js/adaptive-devices';
+import './js/API/fetchAPI';
+import './js/mobile-menu-read';
+import './js/API/api-data-normalaizer';
+import './js/switch-theme';
+// import './js/header';
+// import './js/firebace/firebace-main';
+import './js/switcher-mobile-menu';
